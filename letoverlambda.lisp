9292d4b86ced67ea6e36cccba71b06d91def8bdb
icepic <icepic2009@googlemail.com>
2019-03-17 14:19:34 +0100

    Add more examples

--- letoverlambda.lisp
+++ letoverlambda.lisp
@@ -17,9 +17,73 @@
 
 
 ;;See Onlisp 49
+(defun flatten (x)
+  (labels ((rec (x acc)
+             (cond ((null x) acc)
+                   ((atom x) (cons x acc))
+                   (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
+
+(flatten (group '(a b c d e f g) 2))
+
+;; See Onlisp 58
+
+(defun mkstr (&rest args)
+  (with-output-to-string (s)
+    (dolist (a args) (princ a s))))
+
+(defun symb (&rest args)
+  (values (intern (apply #'mkstr args))))
+
+;; See Lol 15
+;; Calculate factorial
+(defun fact (x)
+  (if (= x 0)
+      1
+      (* x (fact (- x 1)))))
+
+;; See lol 15
+;; binominal coefficient
+(defun choose (n r )
+  (/ (fact n)
+     (fact (- n r))
+     (fact r)))
 
 
 
+;; Lexcial scoping vs dynamic scoping
+;; Dynamic scoping
+(defvar temp-special)
+(setq temp-special 1)
+(defun temp-special-returner ()
+  temp-special)
 
+(let ((temp-special 2))
+  (temp-special-returner))
 
+;; Lexical scoping
+(setq temp-lexical-2 2)
+(defun temp-lexical-returner ()
+  temp-lexical-2)
+(let ((temp-lexical-2 3))
+  (temp-lexical-returner))
 
+;; See lol 35
+(defun block-scanner (trigger-string)
+  (let* ((trig (coerce trigger-string 'list))
+         (curr trig))
+    (lambda (data-string)
+      (let ((data (coerce data-string 'list)))
+        (dolist (c data)
+          (if curr
+              (setq curr
+                    (if (char= (car curr) c)
+                        (cdr curr)
+                        trig)))))
+      (not curr))))
+
+(defvar scanner (block-scanner "jihad"))
+
+(funcall scanner "we will start")
+(funcall scanner "the ji")
+(funcall scanner "had tomorrow.")
