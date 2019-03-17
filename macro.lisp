30e820f8fa650304a72c7d6ffed134d4016c5f2f
icepic <icepic2009@googlemail.com>
2019-03-17 19:22:52 +0100

    Add macro to define ffi enums
    This macro also converts enum values to string

--- macro.lisp
+++ macro.lisp
@@ -1,14 +1,20 @@
+(defmacro enum-to-string (name enum)
+  (let ((value)
+        (str)
+        (forms))
+    (dolist (item enum)
+      (setf value (third item))
+      (setf str (string (second item)))
+      (push `(,value ,str) forms))
+    `(defun ,name (value) (ecase value ,@forms))))
 
-(macroexpand-1 (ffi:def-enum test (:a :b :c)))
-
-
-(defmacro def-enum2 (name values-list &key (separator-string "#"))
+(defmacro def-enum (name values-list &key (separator-string "#"))
   (let ((constants '())
         (value -1)
         field
+        convert
         forms)
-    (setf 
-          separator-string (string separator-string))
+    (setf separator-string (string separator-string))
     (dolist (item values-list)
       (cond ((symbolp item)
              (setf field item)
@@ -17,45 +23,20 @@
                   (symbolp (setf field (first item)))
                   (integerp (setf value (second item)))
                   (endp (cddr item))))
-            (t
-             (error "Not a valid argument to DEF-ENUM~%~a" values-list)))
+            (t (error "Not a valid argument to DEF-ENUM~%~a" values-list)))
       (setf field (concatenate 'string
                                (symbol-name name)
-                   xo            separator-string
+                               separator-string
                                (string field)))
       (push `(defconstant ,(intern field (symbol-package name))
-               ',value)
-            forms))
+               ',value) forms))
+    (setf convert (concatenate 'string (string-upcase (symbol-name name)) "-TO-STRING" ))
+    (push `(enum-to-string ,(intern convert) ,forms) forms )
     `(progn
        (ffi:def-foreign-type ,name :int)
        ,@forms)))
 
+(DEF-ENUM test4 (:A :B :C :f) :separator-string "-")
 
-(setq x '(PROGN
-          (FFI:DEF-FOREIGN-TYPE TEST2 :INT)
-          (DEFCONSTANT |TEST2#C| '2)
-          (DEFCONSTANT |TEST2#B| '1)
-          (DEFCONSTANT |TEST2#A| '0)))
-
-
-(defmacro def-enum3 (name values-list &key (separator-string "#"))
-  `(to-enum `(ffi:def-enum ,name ,values-list )))
-
-(defmacro def-enum4 (name values-list)
-  `(to-enum (macroexpand (ffi:def-enum ,name ,values-list))))
-
-(defmacro enum-to-string (enum)
-  (let ((value)
-        (str)
-        (forms))
-    (dolist (item (cddr (macroexpand enum)))
-      (setf value (third item))
-      (setf str (string (second item)))
-      (push `(,value ,str) forms))
-    `(defun te (value) (ecase value ,@forms))))
-
-(enum-to-string (FFI:DEF-ENUM TEST4 (:A :B :C)))
-
-(te 2)
 
 
