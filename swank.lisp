882564014c6ae2ebdaf564eaf5a902ed8a57463e
IcePic <icepic1984@gmx.de>
2019-03-21 22:55:00 +0100

    Add swank example

---
+++ swank.lisp
@@ -0,0 +1,31 @@
+(defmacro continuable (&body body)
+  `(restart-case (progn ,@body)
+     (continue () :report "FUCK")))
+
+(defun update-swank ()
+  (continuable
+   (let ((connection (or swank::*emacs-connection*
+                         (swank::default-connection))))
+     (when connection (swank::handle-requests connection t)))))
+
+(defparameter *running* t)
+
+(defun toggle-running ()
+  (if *running* (setq *running* nil)
+      (setq *running*  t)))
+
+(let ((counter 0))
+  (loop while *running* do
+       (when (> 100000 counter)
+         (print "Running")
+         (setq counter 0)
+         (print counter))
+       (update-swank)
+       (sleep 1/10)
+       (incf counter)))
+
+(toggle-running)
+
+
+
+
