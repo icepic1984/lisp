280b2adabc1c80cb0c004623799b7db97a3773d7
Christoph Buck <buck.christoph+git@googlemail.com>
2020-01-16 18:28:45 +0100

    Add generic aux test cases

--- learning_lisp/generic.lisp
+++ learning_lisp/generic.lisp
@@ -24,12 +24,86 @@
       (incf (balance account) overdraft))
     (call-next-method)))
 
+(defmethod withdraw :before ((account checking-account) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (withdraw (overdraft-account account) overdraft)
+      (incf (balance account) overdraft))))
+
 
 (defparameter *account* (make-instance 'bank-account :balance 100))
 
-(defparameter *backup* (make-instance 'checking-account :overdraft-account *account* :balance 120))
+(defparameter *backup*
+  (make-instance 'checking-account
+                 :overdraft-account *account*
+                 :balance 120))
 
-(balance *account*)
-(balance *backup*)
+(defclass a ()
+  ((value :accessor value :initarg :value)))
 
-(withdraw *backup* 130)
+(defclass b (a)
+  ())
+
+(defclass c (b)
+  ())
+
+(find-class 'a)
+
+
+(defgeneric test (object))
+
+(defmethod test ((object a))
+  (format t "A~%"))
+
+(defmethod test ((object b))
+  (format t "B~%"))
+
+(defmethod test ((object c))
+  (format t "C~%"))
+
+(defgeneric test-2 (object))
+
+(defmethod test-2 ((object a))
+  (format t "Primary A~%"))
+
+(defmethod test-2 :before ((object a))
+  (format t "Before A~%"))
+
+(defmethod test-2 :after ((object a))
+  (format t "After A~%"))
+
+(defmethod test-2 :before ((object b))
+  (format t "Before B~%"))
+
+(defmethod test-2 :before ((object c))
+  (format t "Before C~%"))
+
+(defmethod test-2 :after ((object b))
+  (format t "After B~%"))
+
+(defmethod test-2 :after ((object c))
+  (format t "After C~%"))
+
+(defmethod test-2 ((object b ))
+  (format t "Primary B~%")
+  (call-next-method))
+
+(defmethod test-2 ((object c))
+  (format t "Primary C~%")
+  (call-next-method))
+
+(defmethod test-2 :around ((object c))
+  (format t "Around C~%")
+  (call-next-method))
+
+(defmethod test-2 :around ((object b))
+  (format t "Around B~%")
+  (call-next-method))
+
+(defun factory (obj)
+  (make-instance obj :value 100))
+
+(progn
+  (format t "~%")
+  (test-2 (factory 'c)))
+
