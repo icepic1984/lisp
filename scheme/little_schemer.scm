8ea118ca82dec6bd132eef0cf68f9d849f4f923f
Christoph Buck <buck.christoph+git@googlemail.com>
2020-07-06 13:39:55 +0200

    Add more functions

--- scheme/little_schemer.scm
+++ scheme/little_schemer.scm
@@ -39,6 +39,91 @@
    ((eq? old (car l)) (cons new (cons old (cdr l))))
    (#t (cons (car l) (insertL new old (cdr l))))))
 
+(define (subset new old l)
+  (cond
+   ((null? l) '())
+   ((eq? old (car l))
+    (cons new (cdr l)))
+   (#t (cons (car l) (subset new old (cdr l))))))
+
+(define (add1 n)
+  (+ n 1))
+
+(define (sub1 n)
+  (- n 1))
+
+(define (plus1 n m)
+  (cond
+   ((zero? m) n)
+   (else (add1 (plus1 n (sub1 m))))))
+
+(define (mul n m)
+  (cond
+   ((zero? m) 0)
+   (else (plus1 n (mul n (sub1 m))))))
+
+(define (minus1 n m)
+  (cond
+   ((zero? m) n)
+   (else (sub1 (minus1 n (sub1 m))))))
+
+(define (addtub tub)
+  (cond
+   ((null? tub) 0)
+   (else
+    (plus1 (car tub) (addtub (cdr tub))))))
+
+(define (mul2 n m)
+  (cond
+   ((zero? m) 1)
+   (else (mul n (mul2 n (sub1 m))))))
+
+(define (tub+ tub1 tub2)
+  (cond
+   ((null? tub1) tub2)
+   ((null? tub2) tub1)
+   (else
+    (cons (plus1 (car tub1) (car tub2)) (tub+ (cdr tub1) (cdr tub2))))))
+
+(define (gt n m)
+  (cond
+   ((zero? n) #f)
+   ((zero? m) #t)
+   (else (gt (sub1 n) (sub1 m)))))
+
+(define (lt n m)
+  (cond
+   ((zero? m) #f)
+   ((zero? n) #t)
+   (else (lt (sub1 n) (sub1 m)))))
+
+(define (eq n m)
+  (cond
+   ((zero? n) (zero? m))
+   ((zero? m) #f)
+   (else (eq (sub1 n) (sub1 m)))))
+
+(define (div n m)
+  (cond
+   ((< n m) 0)
+   (else (add1 (div (- n m) m)))))
+
+(define (length lat)
+  (cond
+   ((null? lat) 0)
+   (else (add1 (length (cdr lat))))))
+
+(define (pick n lat)
+  (cond
+   ((zero? n) (car lat))
+   (else (pick (sub1 n) (cdr lat)))))
+
+(define (rempick n lat)
+  (cond
+   ((zero? (sub1 n)) (cdr lat))
+   (else
+    (cons (car lat) (rempick (sub1 n) (cdr lat))))))
+
 (atom? 'a)
 (atom? '())
 (atom? '(a b))
@@ -62,4 +147,32 @@
 
 (insertL 'a 'b '(a b c d e))
 (insertR 'a 'b '(a b c d e))
+
+(subset 'a 'b '(a b c d e))
+(subset 'a 'b '())
+(subset 'a 'b '(b c))
+
+
+(add1 10)                               ;11
+(sub1 10)                               ;9
+(plus1 10 10)                           ;20
+(mul 10 3)
+(addtub '(10 20 30))
+
+(tub+ '(10 20 30) '(10 20 30))
+(tub+ '(10 20 30) '(20 30))
+(tub+ '(10 20) '(10 20 30))
+
+(gt 10 2)
+(gt 2 10)
+(gt 10 10)
+
+(lt 10 2)
+(lt 2 10)
+(lt 10 10)
+
+(eq 10 2)
+(eq 2 10)
+(eq 10 10)
+
 
